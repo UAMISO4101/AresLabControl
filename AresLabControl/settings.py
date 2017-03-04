69709324f869c4a5688c8df4d1dc083f2862b699
Oscar Andres Fajardo <oscar8500@gmail.com>
2017-03-03 20:58:19 -0500

    Statics
    Configuration for staticfiles (again)

--- AresLabControl/settings.py
+++ AresLabControl/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
-
 STATIC_URL = '/static/images/'
-STATIC_ROOT = 'staticfiles'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/images"),
 ]
