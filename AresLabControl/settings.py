c23b0561d9f064dbb166a31c37be3a04155cff9f
Oscar Andres Fajardo <oscar8500@gmail.com>
2017-03-03 21:07:20 -0500

    ...
    ...

--- AresLabControl/settings.py
+++ AresLabControl/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
-STATIC_URL = '/static/images/'
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/images"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
