cfdb0a133a82d4f3ce56f89d9e1162fe82a1b7e2
Omar Camacho <omarcz27@gmail.com>
2017-03-14 14:37:14 -0500

    implementacion agregar lugar de almacenamiento

--- LabModule/urls.py
+++ LabModule/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     # Peticiones a vistas
     url(r'^$', views.home, name='home'),
-    url(r'^agregarLugar/$', views.agregar_lugar, name='agregarLugar'),
     url(r'^login/$', views.home, name='login'),
     url(r'^logout/$', views.home, name='logout'),
     url(r'^register/$', views.home, name='register'),
+
+    # Lugar de almacenamiento
+    url(r'^agregarLugar/$', views.agregar_lugar, name='agregarLugar'),
 ]
