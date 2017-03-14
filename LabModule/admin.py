7c421e53676f8578ffa817208fd84e879a62cf12
Oscar Andres Fajardo <oscar8500@gmail.com>
2017-03-14 00:50:13 -0500

    Acceso
    Se crea manejo de cuentas.
    Se realiza control de Navbar

--- LabModule/admin.py
+++ LabModule/admin.py
@@ -1,1 +1,28 @@
+from django.contrib import admin
+
+from .models import UserRole, UserProfile, IdType
+
+
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__"]
+
+    class Meta:
+        model = UserRole
+
+
+class IdTypeAdmin(admin.ModelAdmin):
+    class Meta:
+        model = IdType
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "userCode", "user", "userRole"]
+
+    class Meta:
+        model = UserProfile
+
+
 # Register your models here.
+admin.site.register(UserRole, UserRoleAdmin)
+admin.site.register(IdType, IdTypeAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
