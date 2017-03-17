21cc06fe8c3d97bf6ccc8af9fbab1ab1f3b3896c
Omar Camacho <omarcz27@gmail.com>
2017-03-16 23:19:05 -0500

    modificación y ajustes para los lugares de almacenamiento

--- LabModule/views.py
+++ LabModule/views.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
@@ -19,42 +18,56 @@ def home(request):
     context = {}
     return render(request, "home.html", context)
 
-def agregar_lugar(request):
 
-    return render(request, 'LugarAlmacenamiento/agregar.html')
+def agregar_lugar(request):
+    if request.method == 'POST':
+        form = LugarAlmacenamientoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        form = LugarAlmacenamientoForm()
+
+    return render(request, 'LugarAlmacenamiento/agregar.html', {'form': form})
+
 
 def agregar_maquina(request):
-
     return render(request, 'Maquinas/agregar.html')
+
 
 class MaquinaForm(ModelForm):
     class Meta:
         model = MaquinaProfile
-        fields = ['nombre','laboratorio','descripcion','con_reserva','activa','idSistema','xPos','yPos','imagen']
+        fields = ['nombre', 'laboratorio', 'descripcion', 'con_reserva', 'activa', 'idSistema', 'xPos', 'yPos',
+                  'imagen']
 
-def maquina_create(request,template_name = 'Maquinas/agregar.html'):
+
+def maquina_create(request, template_name='Maquinas/agregar.html'):
     if request.user.is_authenticated() and request.user.has_perm("account.can_addMachine"):
-        form = MaquinaForm(request.POST or None,request.FILES)
-        section={}
-        section['title']='Agregar máquina'
+        form = MaquinaForm(request.POST or None, request.FILES)
+        section = {}
+        section['title'] = 'Agregar máquina'
         section['agregar'] = True
         if form.is_valid():
             new_maquina = form.save()
-            return redirect(reverse('maquina-update',kwargs={'pk':new_maquina.pk}))
-        return render(request, template_name, {'form': form,'section':section})
+            return redirect(reverse('maquina-update', kwargs={'pk': new_maquina.pk}))
+        return render(request, template_name, {'form': form, 'section': section})
     else:
         return HttpResponse('No autorizado', status=401)
+
+
 def maquina_update(request, pk, template_name='Maquinas/agregar.html'):
     if request.user.is_authenticated() and request.user.has_perm("account.can_edditMachine"):
         server = get_object_or_404(MaquinaProfile, pk=pk)
         form = MaquinaForm(request.POST or None, request.FILES or None, instance=server)
-        section={}
-        section['title']='Modificar máquina'
+        section = {}
+        section['title'] = 'Modificar máquina'
         section['agregar'] = False
         if form.is_valid():
             form.save()
-            return redirect(reverse('maquina-update',kwargs={'pk':pk}))
-        return render(request, template_name, {'form':form,'section':section,'server':server})
+            return redirect(reverse('maquina-update', kwargs={'pk': pk}))
+        return render(request, template_name, {'form': form, 'section': section, 'server': server})
     else:
         return HttpResponse('No autorizado', status=401)
 
@@ -72,4 +85,3 @@ def maquina_update(request, pk, template_name='Maquinas/agregar.html'):
 
 class UserRegistrationView(RegistrationView):
     form_class = UserProfileForm
-
